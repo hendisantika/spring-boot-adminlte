341b0f0703e0925087cd6812afa0d0eda231aa93
Hendi Santika <hendisantika@gmail.com>
2017-08-05 11:03:07 +0700

    #9 - Added Abstract Model

---
+++ src/main/java/com/hendisantika/adminlte/model/AbstractModel.java
@@ -0,0 +1,48 @@
+package com.hendisantika.adminlte.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class AbstractModel<Long extends Serializable> implements Serializable {
+
+    private static final long serialVersionUID = -6323358535657100144L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractModel<?> other = (AbstractModel<?>) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
