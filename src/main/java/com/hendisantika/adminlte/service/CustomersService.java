c7f2ea21f14b2f2305410a0341b59910ff6915f2
Hendi Santika <hendisantika@gmail.com>
2017-08-05 11:06:17 +0700

    #12 - Added Customer Service

---
+++ src/main/java/com/hendisantika/adminlte/service/CustomersService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.adminlte.service;
+
+import com.hendisantika.adminlte.model.Customers;
+import com.hendisantika.adminlte.repository.CustomersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomersService extends AbstractService<Customers, Long> {
+
+    @Autowired
+    private CustomersRepository customersRepository;
+
+    @Override
+    protected JpaRepository<Customers, Long> getRepository() {
+        return customersRepository;
+    }
+
+}
