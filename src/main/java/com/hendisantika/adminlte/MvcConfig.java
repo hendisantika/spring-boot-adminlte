c2cbd0e34fc11cbd868da3f021008f766769d7e0
Hendi Santika <hendisantika@gmail.com>
2017-08-07 09:23:30 +0700

    #13 - Added MVC Config

---
+++ src/main/java/com/hendisantika/adminlte/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.hendisantika.adminlte;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("dashboard");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
