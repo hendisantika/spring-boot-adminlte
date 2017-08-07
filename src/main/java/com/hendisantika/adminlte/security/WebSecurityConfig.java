25b96a739f874f44cda76baf45465fe196d7e5e0
Hendi Santika <hendisantika@gmail.com>
2017-08-07 09:39:25 +0700

    #14 - Added Web Security Config

---
+++ src/main/java/com/hendisantika/adminlte/security/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.hendisantika.adminlte.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource datasource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/bootstrap/**", "/dist/**", "/plugins/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .failureUrl("/login?error")
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        //Use Spring Boots User detailsMAnager
+        JdbcUserDetailsManager userDetailsService = new JdbcUserDetailsManager();
+
+        //Set our Datasource to use the one defined in application.properties
+        userDetailsService.setDataSource(datasource);
+
+        //Create BCryptPassword encoder
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        //add components
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+        auth.jdbcAuthentication().dataSource(datasource);
+
+        // add new user "user" with password "password" - password will be encrypted
+        if(!userDetailsService.userExists("bruno")) {
+            List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+            authorities.add(new SimpleGrantedAuthority("USER"));
+            User userDetails = new User("bruno", encoder.encode("1234"), authorities);
+            userDetailsService.createUser(userDetails);
+        }
+    }
+
+}
