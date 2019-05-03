95464d92fde7fa64217122f1c539a0797ba2ae06
Han Ni Ni Myint Thu <HanNiNiMyintThu@startinpoint.com>
2019-05-03 10:54:59 +0630

    customer modified

--- src/main/java/com/hendisantika/adminlte/model/Customers.java
+++ src/main/java/com/hendisantika/adminlte/model/Customers.java
@@ -27,4 +27,54 @@ public class Customers extends AbstractModel<Long> {
     @Column(name = "added_date", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
     private Date addedDate;
 
+    
+    
+	public Customers() {
+		super();
+	}
+
+	
+	public Customers(String firstname, String lastname, String email, Date addedDate) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.addedDate = addedDate;
+	}
+
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+    
+    
+
 }
