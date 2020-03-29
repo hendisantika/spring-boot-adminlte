755f82f480eefd0ef30688024267166c32205219
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-29 07:52:58 +0700

    #50 - Bug Fix Migration Script

--- src/main/resources/db/migration/V1__Init_Table.sql
+++ src/main/resources/db/migration/V1__Init_Table.sql
@@ -1,22 +1,22 @@
-CREATE TABLE users (
-  username VARCHAR(50)  NOT NULL PRIMARY KEY,
-  password VARCHAR(255) NOT NULL,
-  enabled  BOOLEAN      NOT NULL
-)
-  ENGINE = InnoDb;
-CREATE TABLE authorities (
-  username  VARCHAR(50) NOT NULL,
-  authority VARCHAR(50) NOT NULL,
-  FOREIGN KEY (username) REFERENCES users (username),
-  UNIQUE INDEX authorities_idx_1 (username, authority)
-)
-  ENGINE = InnoDb;
+CREATE TABLE users
+(
+    username VARCHAR(50)  NOT NULL PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    enabled  BOOLEAN      NOT NULL
+) ENGINE = InnoDb;
+CREATE TABLE authorities
+(
+    username  VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users (username),
+    UNIQUE INDEX authorities_idx_1 (username, authority)
+) ENGINE = InnoDb;
 
-CREATE TABLE customers (
-  id         BIGINT(50)                          NOT NULL PRIMARY KEY,
-  firstname  VARCHAR(50)                         NOT NULL,
-  lastname   VARCHAR(255)                        NOT NULL,
-  email      VARCHAR(255)                        NOT NULL UNIQUE,
-  added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-)
-  ENGINE = InnoDb;
+CREATE TABLE customers
+(
+    id         BIGINT(50) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    firstname  VARCHAR(50)                         NOT NULL,
+    lastname   VARCHAR(255)                        NOT NULL,
+    email      VARCHAR(255)                        NOT NULL UNIQUE,
+    added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+) ENGINE = InnoDb;
