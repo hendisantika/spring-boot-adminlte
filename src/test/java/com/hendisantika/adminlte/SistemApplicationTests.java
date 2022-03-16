d17f93d44b423e962713c10586c0441f0c8034c3
Jackson <pinkycho1221@gmail.com>
2022-03-16 22:15:15 +0800

    fix some problems
    1. flywaydb downgrade to support mysql5.7
    2. remove ThymeleafConfig according to https://ultraq.github.io/thymeleaf-layout-dialect/migrating-to-3.0/#automatic-module-name-and-package-restructure
    3. remove junit4 annotations
    4. add th:remove="tag" to prevent header going off

--- src/test/java/com/hendisantika/adminlte/SistemApplicationTests.java
+++ src/test/java/com/hendisantika/adminlte/SistemApplicationTests.java
@@ -1,12 +1,9 @@
 package com.hendisantika.adminlte;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
+@SpringBootTest
 public class SistemApplicationTests {
 
 	@Test
